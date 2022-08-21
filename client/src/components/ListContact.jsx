f6d1a9a63515adf3a1233ee81cf4c3e41e0c52fc
Shivam Pandey <sap0442@gmail.com>
2022-08-21 12:07:18 +0530

    added name in the list

--- client/src/components/ListContact.jsx
+++ client/src/components/ListContact.jsx
@@ -5,11 +5,16 @@ const List = ({ contacts }) => {
   return (
     <div>
       {contacts.users.map((e) => (
-        <div key={e.id} style={{border:"2px solid gray",width:"300px",height:"60px",margin:"auto",display:"flex",alignItems:"center",justifyContent:"center",borderRadius:"2px",gap:"10px"}}>
+        <div key={e.id} style={{border:"2px solid gray",width:"500px",height:"60px",margin:"auto",display:"flex",alignItems:"center",justifyContent:"center",borderRadius:"2px",gap:"10px"}}>
           <Link style={{textDecoration:"none"}}
             to={`/info?contact=${e.contact_no}&first_name=${e.first_name}&last_name=${e.last_name}`}
           >
-            <strong>{e.contact_no}</strong>
+            <strong>Name: </strong>{e.first_name+" "+e.last_name}
+          </Link>
+          <Link style={{textDecoration:"none"}}
+            to={`/info?contact=${e.contact_no}&first_name=${e.first_name}&last_name=${e.last_name}`}
+          >
+            <strong>Contact: </strong>{e.contact_no}
           </Link>
           <Link style={{textDecoration:"none"}} to={`/info?contact=${e.contact_no}&first_name=${e.first_name}&last_name=${e.last_name}`}> <button>Send</button> </Link>
         </div>
